1b92ffd7ca7de10116a48cb12b87109cd154d4ea
candle0701 <18867946@qq.com>
2019-04-23 11:35:38 +0800

    vue后台管理框架

---
+++ config/dev.env.js
@@ -0,0 +1,5 @@
+module.exports = {
+  NODE_ENV: '"development"',
+  ENV_CONFIG: '"dev"',
+  BASE_API: '"http://localhost:8011/api/v1"'
+}
---
+++ config/sit.env.js
@@ -0,0 +1,5 @@
+module.exports = {
+  NODE_ENV: '"production"',
+  ENV_CONFIG: '"sit"',
+  BASE_API: '"http://localhost:8011/api/v1"'
+}
